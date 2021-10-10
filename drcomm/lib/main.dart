35846c8f11911445845a685e6e4dfcb70dd6bb98
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2021-10-10 10:14:43 +0530

    final

--- drcomm/lib/main.dart
+++ drcomm/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Screens/Login.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -18,21 +19,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    const bool kIsWeb = identical(0, 0.0);
-    bool check = false;
-    if (kIsWeb == false) {
-      final _auth = FirebaseAuth.instance;
 
-      if (_auth.currentUser == null) {
-        check = false;
-        print("NOt LOGGED IN");
-      } else {
-        check = true;
-        print("Logged in");
-        setState(() {});
-        setState(() {});
-      }
-    }
+    bool check = false;
+  
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
